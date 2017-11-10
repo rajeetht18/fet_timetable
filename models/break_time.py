ad16dad40e20c2953d33ebddfc4d97c36e6d0d6c
rajeetht18 <rajeetht18@gmail.com>
2017-11-10 11:44:14 +0530

    break changes

--- models/break_time.py
+++ models/break_time.py
@@ -7,28 +7,30 @@ class BreaksTime(models.Model):
     _description = 'Break Times'
 
     @api.model
+    def create(self, values):
+        if len(values['break_line_ids']) == 0 :
+            raise UserError(_("Please configure Timetable Days to create your Break Time Constraint."))
+        res = super(BreaksTime, self).create(values)
+        return res
+        
+    @api.model
     def default_line(self):
         period_list = []
         period_dict = {}
         day_config = self.env['timetable.days.config'].search([], order='id desc', limit=1)
-        if day_config:
-            period_dict = {
-                        'is_monday': day_config.tt_monday, 
-                        'is_tuesday': day_config.tt_tuesday,
-                        'is_wednesday': day_config.tt_wednesday, 
-                        'is_thursday': day_config.tt_thursday, 
-                        'is_friday': day_config.tt_friday, 
-                        'is_saturday': day_config.tt_saturday, 
-                        'is_sunday': day_config.tt_sunday
-                    }
-        else:
-            UserError(_("Please configure Timetable Days to create your Break Time Constraint."))
-            return False
         for time in self.env['op.timing'].search([]):
-            period_dict['name'] = time.name
-            period_list.append((0,0,period_dict))
-            period_dict['name'] = ''
-        print period_list,' period_list'
+            if day_config:
+                period_dict = {
+                    'name': time.name,
+                    'is_monday': day_config.tt_monday, 
+                    'is_tuesday': day_config.tt_tuesday,
+                    'is_wednesday': day_config.tt_wednesday, 
+                    'is_thursday': day_config.tt_thursday, 
+                    'is_friday': day_config.tt_friday, 
+                    'is_saturday': day_config.tt_saturday, 
+                    'is_sunday': day_config.tt_sunday
+                }
+                period_list.append((0,0,period_dict))
         return period_list
 
     _sql_constraints = [('unique_name','unique(name)', 'There must be another constraint of this type. Please edit that one.')]
