a95817996ea15edfc1b523dfcf02d74355f5c387
rajeetht18 <rajeetht18@gmail.com>
2017-11-13 10:50:21 +0530

    rajeeth update

--- models/student_faculty.py
+++ models/student_faculty.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
-
 class StudentCourse(models.Model):
     _inherit = 'op.student.course'
 
@@ -13,7 +12,16 @@ class Faculty(models.Model):
     _inherit = 'op.faculty'
 
     class_details = fields.One2many('op.faculty.class.list', 'list_id', string="Splits")
-
+    weight_percent = fields.Float('Weight %', default=100, size=100)
+    max_days = fields.Integer('Max days Per Week', size=10)
+    min_days = fields.Integer('Min days Per Week', size=10)
+    max_gaps = fields.Integer('Max gaps Per Day', size=10)
+    min_gaps = fields.Integer('Min gaps per Day', size=10)
+    max_hrs = fields.Float('Max Hours', size=60)
+    max_hrs_act = fields.Float('Max Hours with Activity', size=60)
+    min_hrs = fields.Float('Min Hours Daily', size=18)
+    max_hrs_cont = fields.Float('Max Hours Continuously', size=18)
+    max_hr_cont_act = fields.Float('Max Hours Continuous with Activity', size=20)
 
 class FacultyClassList(models.Model):
     _name = 'op.faculty.class.list'
