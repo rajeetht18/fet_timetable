e0a6dad1c3e5357f1cff4e3ea7e05907a5354736
Aakash V J <aakashmigp@gmail.com>
2017-11-14 15:37:19 +0530

    models files added

--- models/all_faculty_constraints.py
+++ models/all_faculty_constraints.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class Allfacultyconstraints(models.Model):
@@ -8,8 +9,8 @@ class Allfacultyconstraints(models.Model):
     # Time Constraint II (All Teachers)
     name = fields.Char('Name', default="Faculty Constraint")
     weight_percent = fields.Float('Weight %', default=100, size=100)
-    max_days_per_week = fields.Integer('Max days for All Faculty', size=10)
-    min_days_per_week = fields.Integer('Min days For All Faculty', size=10)
+    max_days_per_week = fields.Integer('Max days Per Week For All Faculty', default='set_default_week', size=10)
+    min_days_per_week = fields.Integer('Min days Per Week For All Faculty', size=10)
     max_gaps_per_day = fields.Integer('Max gaps per day for all Faculty', size=10)
     max_gaps_per_week = fields.Integer('Max gaps per Week for All Faculty', size=10)
     max_hrs_daily = fields.Float('Max Hours daily For All Faculty', size=10)
@@ -17,5 +18,36 @@ class Allfacultyconstraints(models.Model):
     min_hrs_daily = fields.Float('Min Hours Daily For All Faculty', size=10)
     max_hrs_cont_tr = fields.Float('Max Hours Continuously For All Faculty', size=10)
     max_hrs_cont_tr_act = fields.Float('Max Hours Continuously with an activity For All Faculty')
+    interval_start = fields.Many2one('op.timing', 'Interval Start Hour', size=30)
+    interval_end = fields.Many2one('op.timing', 'Interval End Hour', size=30)
 
     _sql_constraints = [('unique_Faculty', 'unique(name)', 'Only one Constraint required.')]
+
+    @api.model
+    def set_default_week(self):
+        default_week = self.env['timetable.days.config'].search[()]
+        for max_days_per_week in default_week:
+            if max_days_per_week == default_week.tt_monday:
+                max_days_per_week.monday = 1
+            if max_days_per_week == default_week.tt_tuesday:
+                max_days_per_week.tuesday = 2
+            if max_days_per_week == default_week.tt_wednesday:
+                max_days_per_week.wednesday = 3
+            if max_days_per_week == default_week.tt_thursday:
+                max_days_per_week.thursday = 4
+            if max_days_per_week == default_week.tt_friday:
+                max_days_per_week.friday = 5
+            if max_days_per_week == default_week.tt_saturday:
+                max_days_per_week.saturday = 6
+            if max_days_per_week == default_week.tt_sunday:
+                max_days_per_week.sunday = 7
+        return max_days_per_week
+
+
+
+    @api.multi
+    @api.constrains('weight_percent')
+    def check_weight_percentage(self):
+        for rec in self:
+            if rec.weight_percent != 100:
+                raise UserError(_("Please set the weight percentage to 100."))
--- models/student_faculty.py
+++ models/student_faculty.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class StudentCourse(models.Model):
@@ -21,9 +22,19 @@ class Faculty(models.Model):
     max_hrs = fields.Float('Max Hours', size=60)
     max_hrs_act = fields.Float('Max Hours with Activity', size=60)
     min_hrs = fields.Float('Min Hours Daily', size=18)
+    interval_start = fields.Many2one('op.timing', 'Interval Start Hour', size=20)
+    interval_end = fields.Many2one('op.timing', 'Interval End Hour', size=25)
     max_hrs_cont = fields.Float('Max Hours Continuously', size=18)
     max_hr_cont_act = fields.Float('Max Hours Continuous with Activity', size=20)
     activity_name = fields.Many2one('op.activity.tags', 'Activity')
+
+    @api.multi
+    @api.constrains('weight_percent')
+    def check_weight_percentage(self):
+        for rec in self:
+            if rec.weight_percent != 100:
+                raise UserError(_("Please set the weight percentage to 100."))
+
 
 class FacultyClassList(models.Model):
     _name = 'op.faculty.class.list'
@@ -34,11 +45,11 @@ class FacultyClassList(models.Model):
         res.name = str(res.id)+" - "+str(res.duration)+" - "+str(res.list_id.name)+" - "+str(res.subject_id.name)+" - "+str(res.activity_tag.name)+" - "+str(res.batch_id.name)
         return res
 
-    name = fields.Char("Name",readonly=1)
+    name = fields.Char("Name", readonly=1)
     list_id = fields.Many2one('op.faculty', 'Faculty Class')
     subject_id = fields.Many2one('op.subject', 'Subject', required=True)
     batch_id = fields.Many2one('op.batch', 'Batch Name', required=True)
     split = fields.Integer('Number of Classes Per Week', size=35)
     weight_percent = fields.Float('Weight %', default=100, size=100)
-    duration = fields.Integer("Duration",default=1)
+    duration = fields.Integer("Duration", default=1)
     activity_tag = fields.Many2many('op.activity.tags', string="Activity Tag")
